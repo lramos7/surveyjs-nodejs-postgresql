e00b1d8adad2982e279df592cdc00a3ce2753ec6
lramos7 <leco.ramos@gmail.com>
2025-10-04 00:36:26 -0300

    Fix: Leitura direta da senha do banco de dados

--- express-app/db-adapters/postgres.js
+++ express-app/db-adapters/postgres.js
@@ -9,9 +9,7 @@ const dbConfig = {
   port: process.env.DATABASE_PORT || 5432,
   database: process.env.DATABASE_DB,
   user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD
-    ? readFileSync(process.env.DATABASE_PASSWORD)
-    : null
+  password: process.env.DATABASE_PASSWORD    
 };
 
 const Pool = require('pg').Pool
